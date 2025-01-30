10b20307f831b339bb25c3d1a76fccdfcda220bd
JulianDou <julian.doutreligne@gmail.com>
2025-01-30 11:34:46 +0100

    ajout box avec js

--- client/script.js
+++ client/script.js
@@ -9,6 +9,10 @@ reponses.push(createReponse(1, "No, it isn't", false));
 let question = createQuestion(1, "Is the sky blue ?", reponses, 1);
 console.log("Question: ", question);
 
-
 let pnjs = createPNJsForQuestion(question);
 console.log("PNJs: ", pnjs);
+
+let aFrame = document.querySelector("a-scene");
+aFrame.innerHTML +=
+    "<a-box position='0 0 -3' rotation='0 45 0' color='#4CC3D9'></a-box>"
+;
