74333e3fd9002e7acb5705114330ec6a599cb083
JulianDou <julian.doutreligne@gmail.com>
2025-02-19 16:00:10 +0100

    pas responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jersey: ['"Jersey 10"', 'cursive'],
+        poppins: ['Poppins', 'sans-serif'],
+        inter: ['inter', 'sans-serif'],
+      },
+      colors: {
+        nav: '#0A1418',
+        background: '#1E2431',
+      }
+    },
   },
   plugins: [],
 }
