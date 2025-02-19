35a341911f75bcd72519f825aa8334a005e51e80
William Brooks <william.brooks@etu.unilim.fr>
2025-02-19 10:45:43 +0100

    Initialisation projet

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',                 // Path to your HTML file
+    './src/**/*.{js,jsx,ts,tsx,html}' // All JS and HTML files in src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
