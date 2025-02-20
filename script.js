f332c1ca30871a84baf3b745d9bfddaa7fc362b1
MaelValin <maelmy19@gmail.com>
2025-02-20 15:46:43 +0100

    finition

--- script.js
+++ script.js
@@ -79,3 +79,16 @@ document.addEventListener("click", (event) => {
 
 
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute("href").substring(1);
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 100, // Ajuste la valeur pour éviter que la navbar ne cache le titre
+                behavior: "smooth"
+            });
+        }
+    });
+});
